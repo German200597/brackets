9069c1556d0725b052d82d025b8d61f4525f6e6b
Герман <german@MacBook-Air-German.local>
2022-03-26 21:49:47 +0300

    first commit

---
+++ brackets.py
@@ -0,0 +1,14 @@
+
+
+сlass Stack:
+    def isEmpty(self):
+        a = []
+        if len(a) == 0:
+            print('True')
+        else:  
+            print('False') 
+    
+
+    def push(self):
+
+
